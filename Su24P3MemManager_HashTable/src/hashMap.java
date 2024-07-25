733b7e05fb281062ac735d24b62e6a3d3c3e6e06
zhimingLi1234567 <149106594+zhimingLi1234567@users.noreply.github.com>
2024-07-25 12:49:58 -0400

    update hashMap kvpair type

--- Su24P3MemManager_HashTable/src/hashMap.java
+++ Su24P3MemManager_HashTable/src/hashMap.java
@@ -1,69 +1,93 @@
 import java.util.*;
 
-public class hashMap {
+public class hashMap
+{
     private KVPair[] table;
     private int size;
-    private final K EMPTYKEY = null;
-    private KVPair<K,V> e = null;
+    private final int EMPTYKEY = -1;
+    private KVPair e = null;
     private static final int INITIAL_CAPACITY = 16;
 
-    public hashMap() {
+    public hashMap()
+    {
         table = new KVPair[INITIAL_CAPACITY];
         size = 0;
     }
 
-    private int hash(K key) {
+
+    private int hash(int key)
+    {
         return Math.abs(key.hashCode()) % table.length;
     }
 
-    public void hashInsert(K key, V value) {
-        if (size >= table.length * 0.5) {
+
+    public void hashInsert(int key, Seminar value)
+    {
+        if (size >= table.length * 0.5)
+        {
             resize();
         }
-        KVPair<K, V> kvPair = new KVPair<>(key, value);
+        KVPair kvPair = new KVPair(key, value);
         int index = hash(key);
-        while (table[index] != null && !table[index].getKey().equals(key)) {
+        while (table[index] != null && !table[index].getKey().equals(key))
+        {
             index = (index + 1) % table.length;
         }
         table[index] = kvPair;
         size++;
     }
 
-    public boolean hashSearch(K K) {
-    	 int home;              // Home position for K
-         int pos = home = h(K); // Initial position is the home slot
-         for (int i = 1;
-              (K != (table[pos]).getKey()) && (EMPTYKEY != (table[pos]).getKey());
-              i++) {
-           pos = (home + p(i)) % size; // Next on probe sequence
-              }
-         if (K == (table[pos]).getKey()) {   // Found it
-           e = table[pos];
-           return true;
-         }
-         else { return false; }            // K not in hash table
-       }
 
-    private void resize() {
-        KVPair<K, V>[] oldTable = table;
+    public boolean hashSearch(K K)
+    {
+        int home;              // Home position for K
+        int pos = home = h(K); // Initial position is the home slot
+        for (int i = 1; (K != (table[pos]).getKey())
+            && (EMPTYKEY != (table[pos]).getKey()); i++)
+        {
+            pos = (home + p(i)) % size; // Next on probe sequence
+        }
+        if (K == (table[pos]).getKey())
+        {   // Found it
+            e = table[pos];
+            return true;
+        }
+        else
+        {
+            return false;
+        }            // K not in hash table
+    }
+
+
+    private void resize()
+    {
+        KVPair[] oldTable = table;
         table = new KVPair[oldTable.length * 2];
         size = 0;
-        for (KVPair<K, V> kvPair : oldTable) {
-            if (kvPair != null) {
+        for (KVPair kvPair : oldTable)
+        {
+            if (kvPair != null)
+            {
                 hashInsert(kvPair.getKey(), kvPair.getValue());
             }
         }
     }
 
-    public int size() {
+
+    public int size()
+    {
         return size;
     }
-    
+
+
     private int h(K key)
     {
-    	return Math.abs(key.hashCode())%table.length;
+        return Math.abs(key.hashCode()) % table.length;
     }
-    private int p(int i) {
+
+
+    private int p(int i)
+    {
         // Your probing function here. For example:
         return (i * i + 1) / 2;
     }
